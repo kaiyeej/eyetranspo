2cea3a71522d2a8ae805d59d89988b2c769ea84f
Kaye Jacildo <58097285+kaiyeej@users.noreply.github.com>
2023-01-05 16:22:10 +0800

    Update modal_user.php

--- pages/users/modal_user.php
+++ pages/users/modal_user.php
@@ -38,7 +38,16 @@
                                     <option value="A">Admin</option>
                                     <option value="U">User</option>
                                 </select>
-                            </div>
+                            </div><div class="form-group">
+                      <label>Single select box using select 2</label>
+                      <select class="js-example-basic-single" style="width: 100%;">
+                        <option value="AL">Alabama</option>
+                        <option value="WY">Wyoming</option>
+                        <option value="AM">America</option>
+                        <option value="CA">Canada</option>
+                        <option value="RU">Russia</option>
+                      </select>
+                    </div>
                         </div>
                         <div class="col-lg-6">
                             <div class="form-group">
