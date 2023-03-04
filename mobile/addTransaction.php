aba8aaa9c2530d1cec12ccd9654892d63329c015
Ginx <songalingginery@gmail.com>
2023-03-04 09:58:24 +0800

    Update addTransaction.php

--- mobile/addTransaction.php
+++ mobile/addTransaction.php
@@ -14,13 +14,19 @@ $destination = $_REQUEST['destination'];
 $fare = getTripFare($_REQUEST['trip_schedule_id']);
 $date = getCurrentDate();
 $response = array();
-$fetch = $mysqli_connect->query("INSERT INTO `tbl_transactions` (`bus_id`, `trip_id`, `user_id`, `fare`, `status`,`date_added`, `destination`) VALUES ('$bus_id', '$trip_id', '$user_id', '$fare', 'P','$date','$destination')");
-if ($fetch) {
-    $response["response"] = 1;
+$get_trips = $mysqli_connect->query("SELECT * FROM tbl_trips WHERE `status`!='A' AND trips='$trip_id'");
+$count = $get_trips->num_rows;
+if ($count > 0) {
+    $fetch = $mysqli_connect->query("INSERT INTO `tbl_transactions` (`bus_id`, `trip_id`, `user_id`, `fare`, `status`,`date_added`, `destination`) VALUES ('$bus_id', '$trip_id', '$user_id', '$fare', 'P','$date','$destination')");
+
+    if ($fetch) {
+        $response["response"] = 1;
+    } else {
+        $fetch["response"] = 0;
+    }
 } else {
     $fetch["response"] = 0;
 }
-
 
 array_push($response_array['array_data'], $response);
 echo json_encode($response_array);
